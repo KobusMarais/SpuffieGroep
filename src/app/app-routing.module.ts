764eb53fe415acc3dd5e1862d08efdfb901de2d1
niella <nagenbag@retrorabbit.co.za>
2017-10-20 12:27:38 +0200

    Fixed routing, styled the header and footer, and added content to the luister page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
+import { KylepComponent } from './components/kylep/kylep.component';
 import { GameComponent } from './components/game/game.component';
-
-import { NgModule, Component } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'game', component: GameComponent },
+  { path: 'listen', component:  KylepComponent },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
 ];
